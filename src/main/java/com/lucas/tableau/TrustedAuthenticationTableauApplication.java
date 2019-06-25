cf7aa1e603abc6247526a4718316866720f4b4b9
Lucas <lucas.alves6540@gmail.com>
2019-06-25 19:58:27 -0300

    Adicionando layout Bootstrap Atacama

---
+++ src/main/java/com/lucas/tableau/TrustedAuthenticationTableauApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.tableau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustedAuthenticationTableauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustedAuthenticationTableauApplication.class, args);
+	}
+
+}
